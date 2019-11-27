f9dd77daf2091a24556200abbc00c0a01a6ec311
merdan <merdan.m@gmail.com>
2019-11-27 17:47:54 +0500

    test ticket

--- config/Wkhtml2pdf.php
+++ config/Wkhtml2pdf.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'debug'       => env('APP_DEBUG', false),
+    'debug'       => false,
     'binpath'     => 'lib/',
     'binfile'     => env('WKHTML2PDF_BIN_FILE', 'wkhtmltopdf-amd64'),
     'output_mode' => 'I',
