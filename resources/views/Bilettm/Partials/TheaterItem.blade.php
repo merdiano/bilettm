7878f1edfd9b2985f4b96b2249207e96b6ecbb8b
merdan <merdan.m@gmail.com>
2019-12-23 17:07:45 +0500

    date localization

--- resources/views/Bilettm/Partials/TheaterItem.blade.php
+++ resources/views/Bilettm/Partials/TheaterItem.blade.php
@@ -3,7 +3,7 @@
         <img class="img-fluid w-100" src="{{asset($event->image_url ?? '#')}}">
         <div class="teator-overlay">
             <div class="texts-wrapper" style="color: #ffffff">
-                <span class="" style="font-size: 30px">{{$event->start_date->format('H:s, d.m.Y')}}</span>
+                <span class="" style="font-size: 30px">{{$event->start_date->formatLocalized('%d %B %H:%m')}}</span>
                 <h2 style="font-size: 60px; line-height: 1">{{$event->title}}</h2>
                 <h5 style="margin-bottom: 20px">{{$event->venue_name}}</h5>
                 <span style="color: #ffffff; font-weight: bold; border: 1px solid #ffffff; border-radius: 5px; padding: 10px 30px; font-size: 20px">Купит билет</span>
