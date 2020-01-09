913848f529727e3f8a4ea620573c74ab0035156b
merdan <merdan.m@gmail.com>
2020-01-09 15:27:16 +0500

    migration for reserved ticks

---
+++ database/migrations/2020_01_09_152339_add_holder_info_to_reserved_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddHolderInfoToReservedTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reserved_tickets', function (Blueprint $table) {
+            $table->string('holder_name')->nullable();
+            $table->string('holder_surname')->nullable();
+            $table->string('holder_email')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reserved_tickets', function (Blueprint $table) {
+            $table->dropColumn('holder_name');
+            $table->dropColumn('holder_surname');
+            $table->dropColumn('holder_email');
+        });
+    }
+}
