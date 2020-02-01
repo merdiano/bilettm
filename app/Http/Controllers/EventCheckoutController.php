d46dfb1468e154a488df62b4f0c1e06d5b577454
merdan <merdan.m@gmail.com>
2020-02-01 17:21:42 +0500

    payment mobile fix5

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -937,7 +937,7 @@ class EventCheckoutController extends Controller
         try {
 
             $order = Order::select('orders.id','order_status_id','is_payment_received','amount','booking_fee',
-                'organiser_booking','event_id','session_id')
+                'organiser_booking_fee','event_id','session_id')
                 ->with(['event:id,sales_volume,organiser_fees_volume,organiser_id'])
                 ->where('transaction_id',$transaction_id)
                 ->where('event_id',$event_id)
