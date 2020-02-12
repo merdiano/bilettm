031a8f9c7aa0bdc519348ff026a78bee9980dd27
merdan <merdan.m@gmail.com>
2020-02-12 16:46:29 +0500

    fixing home slider button3

--- resources/views/desktop/EventsList/subCategoryList.blade.php
+++ resources/views/desktop/EventsList/subCategoryList.blade.php
@@ -15,7 +15,7 @@
                         <div class="container">
                             <div class="row">
                                 @foreach($events as $event)
-                                    @include("desktop.{$category->view_type}")
+                                    @include("desktop.EventsList.{$category->view_type}")
                                 @endforeach
                             </div>
                         </div>
