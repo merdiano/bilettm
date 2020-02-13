2385102db64aac8d740927d9f698405e05435a02
merdan <merdan.m@gmail.com>
2020-02-13 20:03:49 +0500

    mobile version

--- app/Http/Controllers/EventViewController.php
+++ app/Http/Controllers/EventViewController.php
@@ -85,7 +85,7 @@ class EventViewController extends Controller
             }
         }
 
-        return view('desktop.ViewEvent.EventPage', $data);
+        return view('desktop.Pages.EventPage', $data);
     }
 
     /**
