b52ba13a78d93604016e1997b264ee6a734147ab
merdan <merdan.m@gmail.com>
2020-02-18 20:02:50 +0500

    category page mobilee

--- resources/views/mobile/Pages/EventsPage.blade.php
+++ resources/views/mobile/Pages/EventsPage.blade.php
@@ -10,18 +10,15 @@
     @foreach($sub_cats as $cat)
 
         <div class="section-section py-3">
-            <div class="container" style="padding: 0 5% !important;">
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-between">
-                        <h5 >{{$cat->title}}
-                            <div class="title-bottom-line"></div>
-                        </h5>
+            <div class="container">
+                <div class="col-12 d-flex justify-content-between">
+                    <h5 >{{$cat->title}}
+                        <div class="title-bottom-line"></div>
+                    </h5>
 
-{{--                        <a class="red_button" href="{{$cat->url}}">{{__("ClientSide.rep")}}</a>--}}
-                    </div>
-                    @include("Shared.Partials.FilterMenu")
-
+                    {{--                        <a class="red_button" href="{{$cat->url}}">{{__("ClientSide.rep")}}</a>--}}
                 </div>
+                @include("Shared.Partials.FilterMenu")
             </div>
             <div class="owl-carousel owl-theme" id="section-slider{{$loop->iteration}}">
                 @foreach($cat->cat_events as $event)
