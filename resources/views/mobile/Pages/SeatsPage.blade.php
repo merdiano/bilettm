9cd8df49d1ebfb36a28ce2ff9cd9ffbabe48c2d9
merdan <merdan.m@gmail.com>
2020-02-21 19:59:24 +0500

    mobilee pages under checkout

--- resources/views/mobile/Pages/SeatsPage.blade.php
+++ resources/views/mobile/Pages/SeatsPage.blade.php
@@ -4,54 +4,53 @@
         <div class="container">
             <a class="mobile_pt_30 mobile_pb0 collapsed d-flex justify-content-between g-color-main g-text-underline--none--hover g-brd-bottom g-brd-gray-light-v4 g-pa-15-0" href="#accordion-10-body-02" data-toggle="collapse" data-parent="#accordion-10" aria-expanded="false" aria-controls="accordion-10-body-02">
                 <span class="d-flex">
-                    <h1 class="mobile_header_tab" style="font-weight: 600;font-size: 35px;">{{__('ClientSide.seats')}}</h1>
+                    <h2 class="mobile_header_tab">{{__('ClientSide.seats')}}</h2>
                 </span>
             </a>
             <div class="row">
-                <div class="col-md-12">
-                    <div class="pills-struct mt-5">
-                        <ul role="tablist" class="nav nav-pills m-auto w-auto justify-content-center" id="choose_seats">
-                            @foreach($tickets as $ticket)
-                                <li class="active" role="presentation" style="display: inline-block;">
-                                    <a aria-expanded="true" data-toggle="tab" class="@if ($loop->first)active @endif show"
-                                       role="tab" id="home_tab_{{$ticket->id}}" href="#home_{{$ticket->id}}" aria-selected="true">
-                                        {{$ticket->title}} - {{$ticket->price}} TMT.
+                <div class="pills-struct mt-5">
+                    <ul role="tablist" class="nav nav-pills m-auto w-auto justify-content-center" id="choose_seats">
+                        @foreach($tickets as $ticket)
+                            <li class="active" role="presentation" style="display: inline-block;">
+                                <a aria-expanded="true" data-toggle="tab" class="@if ($loop->first)active @endif show"
+                                   role="tab" id="home_tab_{{$ticket->id}}" href="#home_{{$ticket->id}}" aria-selected="true">
+                                    {{$ticket->title}} - {{$ticket->price}} TMT.
 
-                                    </a>
-                                </li>
-                            @endforeach
-                        </ul>
-                        <div class="d-flex mt-5 mb-4" style="width: fit-content; margin: auto">
-                            <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #ebeced; font-size: 13px"></i> {{__('ClientSide.available')}}</span>
-                            <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #69687d; font-size: 13px"></i> {{__('ClientSide.booked')}}</span>
-                            <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #b6b6b6; font-size: 13px"></i> {{__('ClientSide.reserved')}}</span>
-                            <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #ff4159; font-size: 13px"></i> {{__('ClientSide.selection')}}</span>
-                        </div>
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary seats-map" data-toggle="modal" data-target="#exampleModal"
-                                style="background-color: #ffffff; color: #d43d34; border: 1px solid #d43d34; display: block; margin: auto; margin-bottom: 20px; padding: 10px 50px">
-                            @lang("Public_ViewEvent.seats_map")
-                        </button>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <div class="d-flex mt-5 mb-4" style="width: fit-content; margin: auto">
+                        <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #ebeced; font-size: 13px"></i> {{__('ClientSide.available')}}</span>
+                        <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #69687d; font-size: 13px"></i> {{__('ClientSide.booked')}}</span>
+                        <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #b6b6b6; font-size: 13px"></i> {{__('ClientSide.reserved')}}</span>
+                        <span class="text-center" style="width: fit-content; margin: 0 15px; font-size: 18px"><i class="fa fa-circle" style="color: #ff4159; font-size: 13px"></i> {{__('ClientSide.selection')}}</span>
+                    </div>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary seats-map" data-toggle="modal" data-target="#exampleModal"
+                            style="background-color: #ffffff; color: #d43d34; border: 1px solid #d43d34; display: block; margin: auto; margin-bottom: 20px; padding: 10px 50px">
+                        @lang("Public_ViewEvent.seats_map")
+                    </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document" style="max-width: 800px">
-                                <div class="modal-content" style="background-color: unset; border: none; ">
-                                    <div class="modal-header" style="border-bottom: none">
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"
-                                                style="border: 2px solid #ffffff; border-radius: 100px; padding: 0; opacity: 1">
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document" style="max-width: 800px">
+                            <div class="modal-content" style="background-color: unset; border: none; ">
+                                <div class="modal-header" style="border-bottom: none">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                                            style="border: 2px solid #ffffff; border-radius: 100px; padding: 0; opacity: 1">
                                             <span aria-hidden="true"
                                                   style="color: #ffffff; opacity: 1; text-shadow: none; font-weight: lighter; font-size: 35px; padding: 0px !important; width: 30px; height: 30px; display: block; line-height: 31px;">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <img class="img-responsive" src="{{asset('user_content/'.$event->venue->seats_image)}}" style="width: 100%">
-                                    </div>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <img class="img-responsive" src="{{asset('user_content/'.$event->venue->seats_image)}}" style="width: 100%">
                                 </div>
                             </div>
                         </div>
-                        <form id="seats-form" class="ajax" action="{{route('postValidateTickets',['event_id'=>$event->id])}}" method="post">
-                            @csrf
+                    </div>
+                    <form id="seats-form" class="ajax" action="{{route('postValidateTickets',['event_id'=>$event->id])}}" method="post">
+                        @csrf
                         <div class="tab-content" id="choose_seats_content">
                             @foreach($tickets as $ticket)
                                 <div id="home_{{$ticket->id}}" class="tab-pane fade in @if ($loop->first) active show @endif " role="tabpanel">
@@ -60,8 +59,8 @@
                                     <meta property="price"
                                           content="{{ number_format($ticket->price, 2, '.', '') }}">
                                     {{--<div class="row justify-content-center">--}}
-                                        {{--<img style="max-width: 60%;" class="img-responsive" alt="{{$event->venue->venue_name}} - {{$ticket->section->section_no}}"--}}
-                                             {{--src="{{asset('user_content/'.$ticket->section->section_image)}}" >--}}
+                                    {{--<img style="max-width: 60%;" class="img-responsive" alt="{{$event->venue->venue_name}} - {{$ticket->section->section_no}}"--}}
+                                    {{--src="{{asset('user_content/'.$ticket->section->section_image)}}" >--}}
                                     {{--</div>--}}
                                     @if($ticket->is_paused)
                                         <h1 class="text-danger">@lang("Public_ViewEvent.currently_not_on_sale")</h1>
@@ -114,12 +113,11 @@
                         </div>
                         <div class="checked-seats" style="padding: 50px 0; text-align: center">
                             <h5 class="text-center">{{__("ClientSide.yourSeats")}}:</h5>
-                                <div class="your-selected-seats" style="text-align: center; margin-bottom: 50px">
-                                </div>
+                            <div class="your-selected-seats" style="text-align: center; margin-bottom: 50px">
+                            </div>
                             {!!Form::submit(trans("ClientSide.confirm-seats"), ['id' => 'confirm-seats'])!!}
                         </div>
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
 
