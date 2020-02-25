9be336d880c955ce81983d6e4c59d62951e41705
merdan <merdan.m@gmail.com>
2020-02-25 21:21:50 +0500

    additional pages

--- resources/views/desktop/Pages/AboutPage.blade.php
+++ resources/views/desktop/Pages/AboutPage.blade.php
@@ -10,24 +10,24 @@
                 <div class="col-2">
 
 
-                    <ul class="list-group">
-                        <li class="list-group-item"><h3>@lang('ClientSide.about_us')</h3></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'introduction_'.Config::get('app.locale')])}}">{{__('ClientSide.introduction')}}</a></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'partners_'.Config::get('app.locale')])}}">{{__("ClientSide.partners")}}</a></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'contacts_'.Config::get('app.locale')])}}">{{__("ClientSide.contacts")}}</a></li>
+                    <ul class="list-inline">
+                        <li class="list-inline-item text-capitalize"><h3>@lang('ClientSide.about_us')</h3></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'introduction_'.Config::get('app.locale')])}}">{{__('ClientSide.introduction')}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'partners_'.Config::get('app.locale')])}}">{{__("ClientSide.partners")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'contacts_'.Config::get('app.locale')])}}">{{__("ClientSide.contacts")}}</a></li>
 
 
-                        <li class="list-group-item"><h3>@lang('ClientSide.about_payment')</h3></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'how_to_buy_'.Config::get('app.locale')])}}">{{__("ClientSide.how_to_buy")}}</a></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'questions_'.Config::get('app.locale')])}}">{{__("ClientSide.questions")}}</a></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'concert_halls_'.Config::get('app.locale')])}}">{{__("ClientSide.concert_halls")}}</a></li>
-                        <li class="list-group-item"><a href="#">{{__("ClientSide.refund")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><h3>@lang('ClientSide.about_payment')</h3></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'how_to_buy_'.Config::get('app.locale')])}}">{{__("ClientSide.how_to_buy")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'questions_'.Config::get('app.locale')])}}">{{__("ClientSide.questions")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'concert_halls_'.Config::get('app.locale')])}}">{{__("ClientSide.concert_halls")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a href="#">{{__("ClientSide.refund")}}</a></li>
 
 
-                        <li class="list-group-item"><h3>@lang('ClientSide.cooperation')</h3></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'organizers_'.Config::get('app.locale')])}}">{{__("ClientSide.organizers")}}</a></li>
-                        <li class="list-group-item"><a href="{{route('about',['page'=>'oferta_'])}}">{{__("ClientSide.oferta")}}</a></li>
-                        <li class="list-group-item"><a data-toggle="modal" data-target="#exampleModalCenter">{{__("ClientSide.addEvent")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><h3>@lang('ClientSide.cooperation')</h3></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'organizers_'.Config::get('app.locale')])}}">{{__("ClientSide.organizers")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a href="{{route('about',['page'=>'oferta_'])}}">{{__("ClientSide.oferta")}}</a></li>
+                        <li class="list-inline-item text-capitalize"><a data-toggle="modal" data-target="#exampleModalCenter">{{__("ClientSide.addEvent")}}</a></li>
                     </ul>
                 </div>
                 <div class="col-10">
