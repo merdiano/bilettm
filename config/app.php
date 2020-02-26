bd9baa65f2acd449d442c5a7e76f5754b9dad62f
merdan <merdan.m@gmail.com>
2020-02-26 18:46:20 +0500

    booking fee and instagram

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/attendize/public'),
+    'url' => env('APP_URL', 'http://bilettm.com'),
 
     /*
     |--------------------------------------------------------------------------
