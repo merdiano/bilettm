37a9118426ada35563de7a2388af9206d07586a6
merdan <merdan.m@gmail.com>
2020-03-28 15:53:25 +0500

    home page mobile changed

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -72,7 +72,7 @@ class PublicController extends Controller
             ->cat_events()->select('id','sub_category_id','title_tk','title_ru')
             ->onLive($data['start'],$data['end'])
             ->orderBy($order['field'],$order['order'])
-            ->take(8);
+            ->take(9);
 
         foreach ($sub_cats as $sub_cat){
             $events = $sub_cat->cat_events()->select('id','sub_category_id','title_tk','title_ru')
