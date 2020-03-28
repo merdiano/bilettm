22b04a5217db0cb90fba8f65707d0c16c520bd47
merdan <merdan.m@gmail.com>
2020-03-28 15:20:21 +0500

    home page mobile changed

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -59,6 +59,7 @@ class PublicController extends Controller
                     })
                     ->with(['cat_events' => function($q) use($data,$order){
                         $q->onLive($data['start'], $data['end'])
+                            ->take(8)
                             ->orderBy($order['field'],$order['order']);
                     }]);
 //                $query->withLiveEvents($order, $data['start'], $data['end']);
