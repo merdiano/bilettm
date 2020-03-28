1458ad44b176939ee12e98525f0536eaaf3b6f57
merdan <merdan.m@gmail.com>
2020-03-28 13:37:58 +0500

    start of unions

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -35,8 +35,8 @@ class PublicController extends Controller
         $musical = Category::where('view_type','concert')
             ->categoryLiveEvents(8)
             ->take(1)
-            ->unionAll($cartoon,$cinema);
-        $all = $musical->get();
+;
+        $all = $cinema->unionAll($cartoon)->unionAll($musical)->get();
 
         dd($all);
 
