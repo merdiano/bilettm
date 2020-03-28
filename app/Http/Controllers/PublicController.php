b76ec01ae55091420837ac723802a7034b02a0d7
merdan <merdan.m@gmail.com>
2020-03-28 12:39:42 +0500

    Venues public ready for Test

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -26,15 +26,20 @@ class PublicController extends Controller
     public function showHomePage(){
         $cinema = Category::where('view_type','cinema')
             ->categoryLiveEvents(16)
-            ->first();
+            ->limit(1);
 
         $cartoon = Category::where('view_type','exhibition')//todo change to cartoon multik
             ->categoryLiveEvents(16)
-            ->first();
+            ->limit(1)
+            ->union($cinema);
 
-        $musical =Category::where('view_type','concert')
+        $musical = Category::where('view_type','concert')
             ->categoryLiveEvents(8)
-            ->first();
+            ->limit(1)
+            ->union($cartoon);
+        $all = $musical->get();
+
+        dd($all);
 
         $sliders = Slider::where('active',1)
             ->where(Config::get('app.locale'),1)
