b6108af77444b260c956be508331f19621c86eb2
merdan <merdan.m@gmail.com>
2020-04-06 18:12:36 +0500

    admin slider

--- resources/views/mobile/Partials/HomeSlider.blade.php
+++ resources/views/mobile/Partials/HomeSlider.blade.php
@@ -3,6 +3,10 @@
         @foreach($sliders as $slider)
         <div class="item">
             <div class="bg-img" style="background-image: url({{asset($slider->image)}}); padding-top: 41%;">
+                <a href="{{$slider->link ?? '#'}}" class="d-block container g-py-200 h-100" style="top: 0; bottom: 0; left: 0; right: 0; position:absolute;"></a>
+            </div>
+            <div class="slider-content" style="position: absolute;">
+                {{$slider->text}}
             </div>
         </div>
         @endforeach
