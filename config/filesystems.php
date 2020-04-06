307959830d0cdf3a9c6096341fc6aa99c7350039
merdan <merdan.m@gmail.com>
2020-04-06 17:23:39 +0500

    admin users menu

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,11 @@ return [
             'endpoint'  => 'https://identity.api.rackspacecloud.com/v2.0/',
             'region'    => 'IAD',
         ],
-
+        // used for Backpack/BackupManager
+        'backups' => [
+            'driver' => 'local',
+            'root'   => storage_path('backups'), // that's where your backups are stored by default: storage/backups
+        ],
     ],
 
 ];
