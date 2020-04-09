68394d9b8b5bcba0af73c827dc80b57e052c5ad0
merdan <merdan.m@gmail.com>
2020-04-09 17:52:52 +0500

    checkout fail

--- resources/views/desktop/Pages/OrderExpectingPayment.blade.php
+++ resources/views/desktop/Pages/OrderExpectingPayment.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     {{\DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('about',trans('ClientSide.checkout_fail_title'))}}
     <section id="intro" class="container">
-        <div class="row mt-5 pt-5">
-            <div class="justify-content-center my-5 pt-5">
+        <div class="row justify-content-center pt-5">
+            <div class="my-5 pt-5">
                 <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 240 240">
                     <g id="_x31_3-Info_Box" transform="translate(0 0)">
                         <path id="Path_448" data-name="Path 448" d="M110.235.388C52.643,4.939,5.79,51.177.518,108.707a119.8,119.8,0,0,0,13.142,66.9,28.666,28.666,0,0,1,1.448,23.185L0,240l43.655-14.553a28.5,28.5,0,0,1,21.948,1.51,119.809,119.809,0,0,0,66.8,12.418c57.074-5.748,102.679-52.411,107.205-109.594A120.03,120.03,0,0,0,110.235.388Z" transform="translate(0 0)" fill="#d33d33"/>
