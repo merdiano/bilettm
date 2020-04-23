82e585fa21a4524f1eff06898896562d32a5b5a1
merdan <merdan.m@gmail.com>
2020-04-23 15:22:25 +0500

    home page musical fix

--- resources/views/desktop/Partials/concert.blade.php
+++ resources/views/desktop/Partials/concert.blade.php
@@ -1,10 +1,10 @@
 <section id="konserty" class="container" style="background-image: url({{asset('assets/images/bg/konserty.jpg')}});
         background-repeat: no-repeat; background-size: 100%;">
     <div class="tab-header d-flex justify-content-between col-12">
-        <h2 class=""><a class="text-light text-uppercase" href="{{$category->url}}">{{$category->title}}</a></h2>
+        <h2 class="mt-5"><a class="text-light text-uppercase" href="{{$category->url}}">{{$category->title}}</a></h2>
         <div style="height: 5px; margin-left: 5px; position: absolute; bottom: 0px; width: 100px; background-color: #ffffff"></div>
     </div>
-    <div class="tab-ozi col-12">
+    <div class="tab-ozi col-12 pb-5">
        <!-- End Nav tabs -->
         <div class="owl-carousel container row" id="konserty-tab1" style="padding: 0 !important; margin: 0">
 
