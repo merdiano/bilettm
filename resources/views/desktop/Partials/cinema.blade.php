8f66e2d535b6b379206925ec841631bfa3f9d532
medet97 <medet-kadyr2020@mail.ru>
2020-04-24 17:57:38 +0500

    Favicon and instagram icon

--- resources/views/desktop/Partials/cinema.blade.php
+++ resources/views/desktop/Partials/cinema.blade.php
@@ -1,3 +1,11 @@
+@section('after_styles')
+    <link href="{{asset('vendor/gijgo/gijgo.min.css')}}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{asset('vendor/slick-carousel/slick/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/owlcarousel/assets/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/owlcarousel/assets/owl.theme.default.min.css')}}">
+    <link  rel="stylesheet" href="{{asset('vendor/jquery-ui/themes/base/jquery-ui.min.css')}}">
+@endsection
+
 <section id="kinoteator" class="kinoteator-section waaa container mb-5">
     <div class="tab-header d-flex justify-content-between col-12 px-0">
         <h2> <a class="text-dark text-uppercase" href="{{$category->url}}">{{$category->title}}</a></h2>
@@ -27,3 +35,37 @@
         </div>
     </div>
 </section>
+
+@section('after_scripts')
+
+    <script src="{{asset('vendor/gijgo/gijgo.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('vendor/slick-carousel/slick/slick.js')}}"></script>
+    <script src="{{asset('vendor/owlcarousel/owl.carousel.min.js')}}"></script>
+
+    <script>
+        $('#datepicker').datepicker({
+            uiLibrary: 'bootstrap4',
+            icons: {
+                rightIcon: '{{__("ClientSide.date")}} <i class="fa fa-caret-down"></i>'
+            }
+        });
+        $('.owl-carousel').owlCarousel({
+            stagePadding: 70,
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+    </script>
+
+@endsection
