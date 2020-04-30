dbbbc0f474568fadf34aa763187d0e468640a007
medet97 <medet-kadyr2020@mail.ru>
2020-04-30 20:35:54 +0500

    calendar fixes

--- resources/views/desktop/Pages/EventsPage.blade.php
+++ resources/views/desktop/Pages/EventsPage.blade.php
@@ -46,7 +46,8 @@
         $('#datepicker').datepicker({
             uiLibrary: 'bootstrap4',
             icons: {
-                rightIcon: '{{__("ClientSide.date")}} <i class="fa fa-caret-down"></i>'
+                {{--rightIcon: '{{__("ClientSide.date")}} <i class="fa fa-caret-down"></i>'--}}
+                rightIcon: '<i class="fa fa-calendar" style="padding: 7px;background-color: #d33d33;color: #ffffff;border-radius: 5px;margin: -10px;"></i>'
             }
         });
     </script>
