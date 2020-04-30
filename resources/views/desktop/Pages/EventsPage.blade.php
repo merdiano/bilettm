ecddb7e5a32c1e915af919c97e400e2fe0f214f4
medet97 <medet-kadyr2020@mail.ru>
2020-04-30 16:35:20 +0500

    calendar fixes

--- resources/views/desktop/Pages/EventsPage.blade.php
+++ resources/views/desktop/Pages/EventsPage.blade.php
@@ -41,13 +41,13 @@
 @endsection
 
 @section('after_scripts')
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
-        $('#datepicker').datepicker({
-            uiLibrary: 'bootstrap4',
-            icons: {
-                rightIcon: '{{__("ClientSide.date")}} <i class="fa fa-caret-down"></i>'
-            }
-        });
-
+        $( function() {
+            $("#datepicker").datepicker();
+        } );
     </script>
 @endsection
