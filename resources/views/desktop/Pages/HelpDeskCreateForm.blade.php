19e403a8a042d3aab611bdd402b97a8e7d9ae37e
merdan <merdan.m@gmail.com>
2020-04-30 14:44:17 +0500

    help desk crud

--- resources/views/desktop/Pages/HelpDeskCreateForm.blade.php
+++ resources/views/desktop/Pages/HelpDeskCreateForm.blade.php
@@ -2,28 +2,38 @@
 @section('content')
     {{\DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('help')}}
 
-    <section class="movie-items-group firts-child" style="margin-bottom: 100px">
+    <section class="movie-items-group firts-child my-5">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-6">
                     <form action="{{route('help.create')}}" method="POST">
                         @csrf
                         <div class="form-group">
-                            {!! Form::label('title_ru', trans("Event.event_title_ru"), array('class'=>'control-label required')) !!}
-                            {!! Form::text('title_ru', Input::old('title_ru'),array('class'=>'form-control' ))  !!}
+                            {!! Form::label('name', trans("ClientSide.name"), array('class'=>'control-label required')) !!}
+                            {!! Form::text('name', old('name'),array('class'=>'form-control' ))  !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('email', trans("ClientSide.email"), array('class'=>'control-label required')) !!}
+                            {!! Form::text('email', old('email'),array('class'=>'form-control' ))  !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('phone', trans("ClientSide.phone"), array('class'=>'control-label')) !!}
+                            {!! Form::text('phone', old('phone'),array('class'=>'form-control' ))  !!}
+                        </div>
+
+                        <div class="form-group address-automatic">
+                        {!! Form::label('category', trans("ClientSide.category"), array('class'=>'control-label')) !!}
+                        {!! Form::select('category',$categories->pluck('id','title'), old('category'), ['class' => 'form-control','id'=>'category']) !!}
                         </div>
                         <div class="form-group custom-theme">
-                            {!! Form::label('description_ru', trans("Event.event_description_ru"), array('class'=>'control-label required')) !!}
-                            {!! Form::textarea('description_ru', Input::old('description_ru'),
+                            {!! Form::label('text', trans("ClientSide.text"), array('class'=>'control-label required')) !!}
+                            {!! Form::textarea('text', old('text'),
                                         array(
                                         'class'=>'form-control  editable',
                                         'rows' => 5
                                         ))  !!}
                         </div>
-                        <div class="form-group address-automatic">
-                        {!! Form::label('venue_name', trans("Event.venue_name"), array('class'=>'control-label required ')) !!}
-                        {!! Form::select('venue_id',$categories->pluck('id','title'), Input::old('venue_id'), ['class' => 'form-control','id'=>'venue_name']) !!}
-                        </div>
+                        {!! Form::submit(trans("ClientSide.create_ticket"), ['class'=>"btn btn-success"]) !!}
                     </form>
                 </div>
             </div>
