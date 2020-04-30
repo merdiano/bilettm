4b952fe1277ad19bb3046fbe8f649eab7405201b
medet97 <medet-kadyr2020@mail.ru>
2020-04-30 15:29:01 +0500

    calendar fixes

--- resources/views/desktop/Pages/EventsPage.blade.php
+++ resources/views/desktop/Pages/EventsPage.blade.php
@@ -64,7 +64,7 @@
                     //event.oldValue      - Previous value of the modified attribute
                     //event.newValue      - New value of the modified attribute
                     //Triggered when the selected elements attribute is added/updated/removed
-                    alert('hit');
+                    alert($(this).attr('selectedday'));
                 }
             });
         });
