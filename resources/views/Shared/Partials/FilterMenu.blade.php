07c3520b307f93a3fbd4b5f17f7c466413489e85
medet97 <medet-kadyr2020@mail.ru>
2020-05-01 14:17:38 +0500

    calendar fixes

--- resources/views/Shared/Partials/FilterMenu.blade.php
+++ resources/views/Shared/Partials/FilterMenu.blade.php
@@ -1,3 +1,11 @@
+@push('after_styles')
+    <style>
+        .calendar-form button{
+            z-index: 100;
+        }
+    </style>
+@endpush
+
 <nav class="w-100">
     <div class="container">
         <ul class="nav u-nav-v1-1 g-mb-20 category-filter" data-btn-classes="btn btn-md btn-block rounded-0 u-btn-outline-lightgray g-mb-20">
@@ -15,7 +23,7 @@
                 <a class="nav-link" href="{{$category->customUrl(['start'=>now(),'end'=>today()->endOfMonth()])}}">{{__('ClientSide.month')}}</a>
             </li>
 
-            <li class="nav-item dropdown" style="position: relative; margin-left: -20px;">
+            <li class="nav-item dropdown" style="position: relative; margin-left: -10px;">
                 <form action="{{$category->url}}" method="post" class="calendar-form ">
                     @csrf
                     {{--<a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="tab">Дата <i class="fa fa-caret-down"></i></a>--}}
