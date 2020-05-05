731e70326bdf7c27ea320f91d9c0fa3b49bf942e
merdan <merdan.m@gmail.com>
2020-05-05 16:46:35 +0500

    help desk admin

--- resources/views/admin/HelpDeskTicket.blade.php
+++ resources/views/admin/HelpDeskTicket.blade.php
@@ -2,8 +2,8 @@
 @section('header')
     <section class="content-header">
         <h1>
-            <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-            <small>{!! $crud->getSubheading() ?? mb_ucfirst(trans('backpack::crud.preview')).' '.$crud->entity_name !!}.</small>
+            <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name !!}</span>
+            <small>{!! $crud->getSubheading() ?? '#'.$entry->code !!}.</small>
         </h1>
         <ol class="breadcrumb">
             <li><a href="{{ url(config('backpack.base.route_prefix'), 'dashboard') }}">{{ trans('backpack::crud.admin') }}</a></li>
@@ -25,57 +25,33 @@
 
             <!-- Default box -->
             <div class="m-t-20">
-                @if ($crud->model->translationEnabled())
-                    <div class="row">
-                        <div class="col-md-12 m-b-10">
-                            <!-- Change translation button group -->
-                            <div class="btn-group pull-right">
-                                <button type="button" class="btn btn-sm btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} &nbsp; <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    @foreach ($crud->model->getAvailableLocales() as $key => $locale)
-                                        <li><a href="{{ url($crud->route.'/'.$entry->getKey()) }}?locale={{ $key }}">{{ $locale }}</a></li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                @else
-                @endif
                 <div class="box no-padding no-border">
                     <table class="table table-striped">
                         <tbody>
-                        @foreach ($crud->columns as $column)
-                            <tr>
-                                <td>
-                                    <strong>{{ $column['label'] }}</strong>
-                                </td>
-                                <td>
-                                    @if (!isset($column['type']))
-                                        @include('crud::columns.text')
-                                    @else
-                                        @if(view()->exists('vendor.backpack.crud.columns.'.$column['type']))
-                                            @include('vendor.backpack.crud.columns.'.$column['type'])
-                                        @else
-                                            @if(view()->exists('crud::columns.'.$column['type']))
-                                                @include('crud::columns.'.$column['type'])
-                                            @else
-                                                @include('crud::columns.text')
-                                            @endif
-                                        @endif
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                        @if ($crud->buttons->where('stack', 'line')->count())
-                            <tr>
-                                <td><strong>{{ trans('backpack::crud.actions') }}</strong></td>
-                                <td>
-                                    @include('crud::inc.button_stack', ['stack' => 'line'])
-                                </td>
-                            </tr>
-                        @endif
+                        <tr>
+                            <td>
+                                <strong>Name :</strong>
+                            </td>
+                            <td>
+                                {{$entry->name}}
+                            </td>
+                            <td>
+                                <strong>
+                                    Email :
+                                </strong>
+                            </td>
+                            <td>{{$entry->email}}</td>
+                            <td>
+                                Phone :
+                            </td>
+                            <td>{{$entry->phone}}</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Subject : </strong></td>
+                            <td>{{$entry->subject}}</td>
+                        </tr>
+                        <tr><td>{{$entry->text}}</td></tr>
+
                         </tbody>
                     </table>
                 </div><!-- /.box-body -->
