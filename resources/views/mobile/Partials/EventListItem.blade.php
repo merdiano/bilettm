9cf65f79b414ef2382b6da5f772ad44a6089b8b9
merdan <merdan.m@gmail.com>
2023-06-12 14:37:14 +0500

    mobile homapage

--- resources/views/mobile/Partials/EventListItem.blade.php
+++ resources/views/mobile/Partials/EventListItem.blade.php
@@ -1,6 +1,6 @@
 <article class="u-block-hover">
     <div class="g-bg-cover">
-        <img class="d-flex align-items-end" src="{{asset($event->image_url ?? '#')}}" style="border-radius: 5px">
+        <img class="d-flex align-items-end" src="{{asset($event->image_url ?? '#')}}" style="border-radius: 5px" height="320px">
     </div>
     <div class="u-block-hover__additional--partially-slide-up h-100 text-center g-z-index-1 mt-auto"
          style="background-image: url({{asset('assets/images/bg/konserty-item.png')}})">
--- resources/views/mobile/Partials/HomeSlider.blade.php
+++ resources/views/mobile/Partials/HomeSlider.blade.php
@@ -2,7 +2,7 @@
     <div class="owl-carousel owl-theme" id="mob-top-slider">
         @foreach($sliders as $slider)
         <div class="item">
-            <div class="bg-img" style="background-image: url({{asset($slider->image)}}); padding-top: 41%;">
+            <div class="bg-img" style="background-image: url({{public_path($slider->image)}}); padding-top: 41%;">
                 <a href="{{$slider->link ?? '#'}}" class="d-block container g-py-200 h-100" style="top: 0; bottom: 0; left: 0; right: 0; position:absolute;"></a>
             </div>
             <div class="slider-content" style="position: absolute; top: 0;">
