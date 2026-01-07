3f2c94fb8279ee8e6d87c272cd64527d1ec5a9b6
merdan <merdan.m@gmail.com>
2026-01-07 12:26:45 +0500

    kupit bilet style

--- resources/views/desktop/Partials/HomeSlider.blade.php
+++ resources/views/desktop/Partials/HomeSlider.blade.php
@@ -23,8 +23,8 @@
                 <!-- End Parallax Image -->
                 <a href="{{$slider->link ?? '#'}}" class="d-block container g-py-200 h-100" style="top: 0; bottom: 0; left: 0; right: 0; position:absolute;"></a>
             </li>              @if($slider->link)
-            <div class="slider-content" style="position: absolute; bottom: 200px; right: 200px; padding: 20px;">
-                <a href="{{$slider->link}}" class="btn btn-danger btn-lg">
+            <div class="slider-content" style="position: absolute; bottom: 100px; right: 100px; padding: 20px;">
+                <a href="{{$slider->link}}" class="btn btn-danger btn-lg px-5 py-3">
                     {{$slider->title}}
                 </a>
             </div>
